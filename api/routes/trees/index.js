0d3616e4d222175927a280026cd36ace102accc1
Umair Jibran <umairjibran7@gmail.com>
2020-11-08 10:45:40 +0500

    add mongodb

--- api/routes/trees/index.js
+++ api/routes/trees/index.js
@@ -1,25 +1,53 @@
+const { json } = require("body-parser");
 const Express = require("express");
-
-const { trees } = require("../../dummy.json");
+const MongoClient = require("mongodb").MongoClient;
 
 const router = Express.Router();
 
+const userName = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+const db = process.env.DB;
+const mongoClient = MongoClient.connect(
+   `mongodb+srv://${userName}:${password}@shaoor-tech.djiac.mongodb.net/${db}?retryWrites=true&w=majority`,
+   { useUnifiedTopology: true }
+);
+
 module.exports = () => {
    router.get("/", (req, res) => {
-      res.status(200).json(trees);
+      mongoClient
+         .then(client => {
+            client
+               .db(db)
+               .collection("trees")
+               .find()
+               .toArray()
+               .then(trees => {
+                  return res.status(200).json(trees);
+               });
+         })
+         .catch(err => {
+            console.log(err);
+            return res.status(500);
+         });
    });
 
    router.get("/:id", (req, res) => {
       // this route is used to fetch single tree
       const treeId = req.params.id;
-      let theTree;
-      trees.forEach(tree => {
-         if (tree.id === treeId) {
-            theTree = tree;
-            return;
-         }
-      });
-      res.status(200).json(theTree);
+      mongoClient
+         .then(client => {
+            client
+               .db(db)
+               .collection("trees")
+               .findOne({ id: treeId })
+               .then(tree => {
+                  return res.status(200).json(tree);
+               });
+         })
+         .catch(err => {
+            console.log(err);
+            return res.status(500);
+         });
    });
 
    return router;
--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -1,25 +1,53 @@
+const { json } = require("body-parser");
 const Express = require("express");
-
-const { users } = require("../../dummy.json");
+const MongoClient = require("mongodb").MongoClient;
 
 const router = Express.Router();
 
+const userName = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+const db = process.env.DB;
+const mongoClient = MongoClient.connect(
+   `mongodb+srv://${userName}:${password}@shaoor-tech.djiac.mongodb.net/${db}?retryWrites=true&w=majority`,
+   { useUnifiedTopology: true }
+);
+
 module.exports = () => {
    router.get("/", (req, res) => {
-      res.status(200).json(users);
+      mongoClient
+         .then(client => {
+            client
+               .db(db)
+               .collection("users")
+               .find()
+               .toArray()
+               .then(users => {
+                  return res.status(200).json(users);
+               });
+         })
+         .catch(err => {
+            console.log(err);
+            return res.status(500);
+         });
    });
 
    router.get("/:id", (req, res) => {
       // this route is used to fetch single user
       const userId = req.params.id;
-      let theUser;
-      users.forEach(user => {
-         if (user.id === userId) {
-            theUser = user;
-            return;
-         }
-      });
-      return res.status(200).json(theUser);
+      mongoClient
+         .then(client => {
+            client
+               .db(db)
+               .collection("users")
+               .findOne({ id: userId })
+               .then(user => {
+                  return res.status(200).json(user);
+               });
+         })
+         .catch(err => {
+            console.log(err);
+            return res.status(500);
+         });
    });
 
    return router;
